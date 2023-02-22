14c6a526de573980ab20331896ca43e5f474633b
vincentvc168 <61619999+vincentvc168@users.noreply.github.com>
2023-02-22 08:03:41 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,6 +1,6 @@
 # SSH
 # https://www.cyberciti.biz/faq/ubuntu-linux-install-openssh-server/
-if ! command -v sshd &> /dev/null
+if service --status-all | grep -Fq 'ssh
 then
     sudo apt update
     sudo apt-get install openssh-server -y
