ff65abee7f9139606c25434cb6bccc7358e92d81
vincentvc168 <61619999+vincentvc168@users.noreply.github.com>
2023-03-03 21:20:11 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -27,7 +27,8 @@ then
     sudo apt-get update -y
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-    sudo usermod -aG docker vmware
+    #sudo usermod -aG docker vmware
+    sudo usermod -aG docker $(whoami)
     newgrp docker
 fi
 
