ad111250662c20987a9c724bc1e5a586ae1d35c7
vincentvc168 <61619999+vincentvc168@users.noreply.github.com>
2023-03-06 00:11:43 +0800

    Update initial.sh
    Move Docker to last execution due to "newgrp docker"

--- initial.sh
+++ initial.sh
@@ -10,29 +10,6 @@ then
     sudo systemctl start ssh
 fi
 
-# Docker
-# https://docs.docker.com/engine/install/ubuntu/
-if ! command -v docker &> /dev/null
-then
-    sudo apt-get update -y
-    sudo apt-get install \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release -y
-    sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update -y
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-    #sudo usermod -aG docker vmware
-    sudo usermod -aG docker $(whoami)
-    newgrp docker
-fi
-
 # Virtualbox
 # https://phoenixnap.com/kb/install-virtualbox-on-ubuntu
 if ! command -v virtualbox &> /dev/null
@@ -75,6 +52,29 @@ then
     sudo apt install apt-transport-https -y
     sudo apt update -y
     sudo apt install code -y # or code-insiders
+fi
+
+# Docker
+# https://docs.docker.com/engine/install/ubuntu/
+if ! command -v docker &> /dev/null
+then
+    sudo apt-get update -y
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release -y
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+    #sudo usermod -aG docker vmware
+    sudo usermod -aG docker $(whoami)
+    newgrp docker
 fi
 
 # minikube start --driver=virtualbox --memory 22288 --cpus 8 --disk-size="70000mb" --kubernetes-version=v1.25.0
