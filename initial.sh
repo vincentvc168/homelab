948a32efe7ae7873d282f9063bbce5cdfd71cc53
vincentvc168 <61619999+vincentvc168@users.noreply.github.com>
2023-03-06 00:13:36 +0800

    Update initial.sh
    Fix non useful "-y" in Visual Studio Code section.

--- initial.sh
+++ initial.sh
@@ -46,7 +46,7 @@ if ! command -v code &> /dev/null
 then
     sudo apt-get install wget gpg -y
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg -y
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
     sudo apt install apt-transport-https -y
