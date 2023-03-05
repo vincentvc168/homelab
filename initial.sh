2105de5620ae3c269b9b794dc746ff0a3e6fb138
vincentvc168 <61619999+vincentvc168@users.noreply.github.com>
2023-03-05 23:20:22 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -65,13 +65,16 @@ fi
 
 # Visual Studio Code
 # https://code.visualstudio.com/docs/setup/linux
-sudo apt-get install wget gpg -y
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg -y
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https -y
-sudo apt update -y
-sudo apt install code -y # or code-insiders
+if ! command -v code &> /dev/null
+then
+    sudo apt-get install wget gpg -y
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg -y
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+    sudo apt install apt-transport-https -y
+    sudo apt update -y
+    sudo apt install code -y # or code-insiders
+fi
 
 # minikube start --driver=virtualbox --memory 22288 --cpus 8 --disk-size="70000mb" --kubernetes-version=v1.25.0
