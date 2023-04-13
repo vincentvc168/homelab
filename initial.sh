a2beef26c4c3df12abd06f9897003a4a59e3b943
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2023-04-13 20:41:40 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -22,7 +22,7 @@ fi
 # https://minikube.sigs.k8s.io/docs/start/
 if ! command -v minikube &> /dev/null
 then
-    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
     sudo install minikube-linux-amd64 /usr/local/bin/minikube
 fi
 
