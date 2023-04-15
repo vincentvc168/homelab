7c2ab1057a19e2f385b63c74023e40beb0e40144
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2023-04-15 13:42:08 +0800

    Create tap_v14x.sh

---
+++ tap/tap_v14x.sh
@@ -0,0 +1,18 @@
+# Preparing credentials and setting environmental variables
+# vincentc2@vmware.com
+export PIVNET_TOKEN
+read -sp "Enter your token: " PIVNET_TOKEN
+
+# Installing Pivnet CLI
+# https://github.com/pivotal-cf/pivnet-cli
+# https://github.com/pivotal-cf/pivnet-cli/releases/tag/v3.0.1
+wget https://github.com/pivotal-cf/pivnet-cli/releases/download/v3.0.1/pivnet-linux-amd64-3.0.1
+sudo install pivnet-linux-amd64-3.0.1 /usr/local/bin/pivnet
+
+# Login Tanzu Network - https://network.pivotal.io
+pivnet login --api-token "$PIVNET_TOKEN"
+
+# Accept EULAs in Tanzu Application Platform and Cluster Essential
+# 
+
+
