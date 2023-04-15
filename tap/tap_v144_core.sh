1d4d8b708c8fb557736812327e3770b459e6fd03
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2023-04-15 14:15:21 +0800

    Update tap_v144_core.sh

--- tap/tap_v144_core.sh
+++ tap/tap_v144_core.sh
@@ -16,30 +16,30 @@ fi
 
 if ! command -v tanzu &> /dev/null
 then
-# Login Tanzu Network - https://network.pivotal.io
-pivnet login --api-token "$PIVNET_TOKEN"
+  # Login Tanzu Network - https://network.pivotal.io
+  pivnet login --api-token "$PIVNET_TOKEN"
 
-# Accept EULAs in Tanzu Application Platform and Cluster Essential
+  # Accept EULAs in Tanzu Application Platform and Cluster Essential
 
-# Installing Tanzu CLI and plugins
-# Finding the following command in Tanzu Network Portal
-pivnet download-product-files --product-slug='tanzu-application-platform' --release-version='1.4.4' --product-file-id=1457672
+  # Installing Tanzu CLI and plugins
+  # Finding the following command in Tanzu Network Portal
+  pivnet download-product-files --product-slug='tanzu-application-platform' --release-version='1.4.4' --product-file-id=1457672
 
-# Create the tanzu directory, if it does not exist
-mkdir -p $HOME/tanzu
+  # Create the tanzu directory, if it does not exist
+  mkdir -p $HOME/tanzu
 
-# This assumes that you have successfully downloaded the file
-# from Tanzu Network to your home directory
-tar -xvf $HOME/tanzu-framework-linux-amd64-*.tar -C $HOME/tanzu
+  # This assumes that you have successfully downloaded the file
+  # from Tanzu Network to your home directory
+  tar -xvf $HOME/tanzu-framework-linux-amd64-*.tar -C $HOME/tanzu
 
-cd $HOME/tanzu
-export TANZU_CLI_NO_INIT=true
-export VERSION=v0.25.4
-sudo install cli/core/$VERSION/tanzu-core-linux_amd64 /usr/local/bin/tanzu
-tanzu plugin install --local cli all
+  cd $HOME/tanzu
+  export TANZU_CLI_NO_INIT=true
+  export VERSION=v0.25.4
+  sudo install cli/core/$VERSION/tanzu-core-linux_amd64 /usr/local/bin/tanzu
+  tanzu plugin install --local cli all
 
-# Confirm successful installation
-tanzu plugin list
+  # Confirm successful installation
+  tanzu plugin list
 fi
 
 
