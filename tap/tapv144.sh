725c0baff415c9f5cb4966b85d7395f137b21174
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2023-04-15 21:23:08 +0800

    Update tapv144.sh

--- tap/tapv144.sh
+++ tap/tapv144.sh
@@ -2,9 +2,9 @@
 export MASTER_IP=$(minikube ip)
 export HARBOR_IP=harbor.hlab.uk
 export TANZUNETWORK_USERNAME
-read -p "Enter your Docker Username: " TANZUNETWORK_USERNAME
+read -p "Enter your Tanzu Network Username: " TANZUNETWORK_USERNAME
 export TANZUNETWORK_PASSWORD
-read -p "Enter your Docker Password: " TANZUNETWORK_PASSWORD
+read -p "Enter your Tanzu Network Password: " TANZUNETWORK_PASSWORD
 
 # The version of TAP being installed
 export TAP_VERSION="1.4.4"
@@ -21,7 +21,7 @@ export INSTALL_REPO=library
 export INGRESS_DOMAIN="$MASTER_IP.nip.io"
 
 # Log in to self Harbor and the VMware Tanzu Network Registry
-docker login $INSTALL_REGISTRY_HOSTNAME -u "$INSTALL_REGISTRY_USERNAME" -p "INSTALL_REGISTRY_PASSWORD"
+docker login $INSTALL_REGISTRY_HOSTNAME -u "$INSTALL_REGISTRY_USERNAME" -p "$INSTALL_REGISTRY_PASSWORD"
 docker login registry.tanzu.vmware.com -u "$TANZUNETWORK_USERNAME" -p "$TANZUNETWORK_PASSWORD"
 
 imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${TAP_VERSION} \
